96223deb43aba3636486a4321ec191eec1d252a7
rajdipthepower <rajdipthepower360@gmail.com>
2025-09-07 21:45:27 +0530

    Add files via upload

--- countrylist.js
+++ countrylist.js
@@ -1,346 +1,94 @@
 const countryList = {
-  AED: "AE", // United Arab Emirates
-  AFN: "AF", // Afghanistan
-  XCD: "AG", // Antigua and Barbuda
-  ALL: "AL", // Albania
-  AMD: "AM", // Armenia
-  ANG: "AN", // Netherlands Antilles (defunct, replaced by multiple)
-  AOA: "AO", // Angola
-  ARS: "AR", // Argentina
-  AUD: "AU", // Australia
-  AZN: "AZ", // Azerbaijan
-  BAM: "BA", // Bosnia and Herzegovina
-  BBD: "BB", // Barbados
-  BDT: "BD", // Bangladesh
-  BGN: "BG", // Bulgaria
-  BHD: "BH", // Bahrain
-  BIF: "BI", // Burundi
-  BMD: "BM", // Bermuda
-  BND: "BN", // Brunei Darussalam
-  BOB: "BO", // Bolivia
-  BRL: "BR", // Brazil
-  BSD: "BS", // Bahamas
-  BWP: "BW", // Botswana
-  BYN: "BY", // Belarus (new ruble)
-  BZD: "BZ", // Belize
-  CAD: "CA", // Canada
-  CDF: "CD", // Congo (Democratic Republic)
-  XAF: "CF", // Central African CFA franc (Central African Republic etc.)
-  CHF: "CH", // Switzerland
-  CLP: "CL", // Chile
-  CNY: "CN", // China
-  COP: "CO", // Colombia
-  CRC: "CR", // Costa Rica
-  CUP: "CU", // Cuba
-  CVE: "CV", // Cabo Verde
-  CZK: "CZ", // Czechia
-  DJF: "DJ", // Djibouti
-  DKK: "DK", // Denmark
-  DOP: "DO", // Dominican Republic
-  DZD: "DZ", // Algeria
-  EGP: "EG", // Egypt
-  ETB: "ET", // Ethiopia
-  EUR: "EU", // Eurozone (France, Germany, etc.)
-  FJD: "FJ", // Fiji
-  FKP: "FK", // Falkland Islands
-  GBP: "GB", // United Kingdom
-  GEL: "GE", // Georgia
-  GHS: "GH", // Ghana
-  GIP: "GI", // Gibraltar
-  GMD: "GM", // Gambia
-  GNF: "GN", // Guinea
-  GTQ: "GT", // Guatemala
-  GYD: "GY", // Guyana
-  HKD: "HK", // Hong Kong
-  HNL: "HN", // Honduras
-  HRK: "HR", // Croatia
-  HTG: "HT", // Haiti
-  HUF: "HU", // Hungary
-  IDR: "ID", // Indonesia
-  ILS: "IL", // Israel
-  INR: "IN", // India
-  IQD: "IQ", // Iraq
-  IRR: "IR", // Iran
-  ISK: "IS", // Iceland
-  JMD: "JM", // Jamaica
-  JOD: "JO", // Jordan
-  JPY: "JP", // Japan
-  KES: "KE", // Kenya
-  KGS: "KG", // Kyrgyzstan
-  KHR: "KH", // Cambodia
-  KMF: "KM", // Comoros
-  KPW: "KP", // North Korea
-  KRW: "KR", // South Korea
-  KWD: "KW", // Kuwait
-  KYD: "KY", // Cayman Islands
-  KZT: "KZ", // Kazakhstan
-  LAK: "LA", // Laos
-  LBP: "LB", // Lebanon
-  LKR: "LK", // Sri Lanka
-  LRD: "LR", // Liberia
-  LSL: "LS", // Lesotho
-  LYD: "LY", // Libya
-  MAD: "MA", // Morocco
-  MDL: "MD", // Moldova
-  MGA: "MG", // Madagascar
-  MKD: "MK", // North Macedonia
-  MMK: "MM", // Myanmar
-  MNT: "MN", // Mongolia
-  MOP: "MO", // Macao
-  MRU: "MR", // Mauritania
-  MUR: "MU", // Mauritius
-  MVR: "MV", // Maldives
-  MWK: "MW", // Malawi
-  MXN: "MX", // Mexico
-  MYR: "MY", // Malaysia
-  MZN: "MZ", // Mozambique
-  NAD: "NA", // Namibia
-  NGN: "NG", // Nigeria
-  NIO: "NI", // Nicaragua
-  NPR: "NP", // Nepal
-  NZD: "NZ", // New Zealand
-  OMR: "OM", // Oman
-  PAB: "PA", // Panama
-  PEN: "PE", // Peru
-  PGK: "PG", // Papua New Guinea
-  PHP: "PH", // Philippines
-  PKR: "PK", // Pakistan
-  PLN: "PL", // Poland
-  PYG: "PY", // Paraguay
-  QAR: "QA", // Qatar
-  RON: "RO", // Romania
-  RSD: "RS", // Serbia
-  RUB: "RU", // Russia
-  RWF: "RW", // Rwanda
-  SAR: "SA", // Saudi Arabia
-  SBD: "SB", // Solomon Islands
-  SCR: "SC", // Seychelles
-  SDG: "SD", // Sudan
-  SEK: "SE", // Sweden
-  SGD: "SG", // Singapore
-  SLL: "SL", // Sierra Leone
-  SOS: "SO", // Somalia
-  SRD: "SR", // Suriname
-  STN: "ST", // São Tomé and Príncipe (new)
-  SVC: "SV", // El Salvador
-  SYP: "SY", // Syria
-  SZL: "SZ", // Eswatini
-  THB: "TH", // Thailand
-  TJS: "TJ", // Tajikistan
-  TMT: "TM", // Turkmenistan
-  TND: "TN", // Tunisia
-  TOP: "TO", // Tonga
-  TRY: "TR", // Türkiye
-  TTD: "TT", // Trinidad and Tobago
-  TWD: "TW", // Taiwan
-  TZS: "TZ", // Tanzania
-  UAH: "UA", // Ukraine
-  UGX: "UG", // Uganda
   USD: "US", // United States
-  UYU: "UY", // Uruguay
-  UZS: "UZ", // Uzbekistan
-  VEF: "VE", // Venezuela (replaced, now VES)
-  VES: "VE", // Venezuela Bolívar Soberano (current)
-  VND: "VN", // Vietnam
-  VUV: "VU", // Vanuatu
-  YER: "YE", // Yemen
+  EUR: "EU", // Eurozone (includes Spain, Netherlands, Portugal, etc.)
+  GBP: "GB", // United Kingdom
+  INR: "IN", // India
+  AUD: "AU", // Australia
+  CAD: "CA", // Canada
+  JPY: "JP", // Japan
+  CNY: "CN", // China
+  CHF: "CH", // Switzerland
+  NZD: "NZ", // New Zealand
+  SEK: "SE", // Sweden
+  NOK: "NO", // Norway
+  DKK: "DK", // Denmark
   ZAR: "ZA", // South Africa
-  ZMW: "ZM", // Zambia
-  ZWL: "ZW"  // Zimbabwe (reintroduced)
+  SGD: "SG", // Singapore
+  HKD: "HK", // Hong Kong
+  KRW: "KR", // South Korea
+  MYR: "MY", // Malaysia
+  THB: "TH", // Thailand
+  IDR: "ID", // Indonesia
+  PHP: "PH", // Philippines
+  MXN: "MX", // Mexico
+  BRL: "BR", // Brazil
+  ARS: "AR", // Argentina
+  RUB: "RU", // Russia
+  TRY: "TR", // Turkey
+  AED: "AE", // UAE
+  SAR: "SA", // Saudi Arabia
+  EGP: "EG", // Egypt
+  NGN: "NG", // Nigeria
+  PKR: "PK", // Pakistan
+  VND: "VN", // Vietnam
+  KES: "KE", // Kenya
+  COP: "CO", // Colombia
+  CLP: "CL", // Chile
+  PLN: "PL", // Poland
+  CZK: "CZ", // Czechia
+  HUF: "HU", // Hungary
+  RON: "RO", // Romania
+  ILS: "IL", // Israel
+  JOD: "JO", // Jordan
+  QAR: "QA", // Qatar
+  KWD: "KW", // Kuwait
+  OMR: "OM", // Oman
 };
 
 const countryNames = {
-  "Afghanistan": "AF",
-  "Albania": "AL",
-  "Algeria": "DZ",
-  "Andorra": "AD",
-  "Angola": "AO",
-  "Antigua and Barbuda": "AG",
-  "Argentina": "AR",
-  "Armenia": "AM",
-  "Australia": "AU",
-  "Austria": "AT",
-  "Azerbaijan": "AZ",
-  "Bahamas": "BS",
-  "Bahrain": "BH",
-  "Bangladesh": "BD",
-  "Barbados": "BB",
-  "Belarus": "BY",
-  "Belgium": "BE",
-  "Belize": "BZ",
-  "Benin": "BJ",
-  "Bhutan": "BT",
-  "Bolivia": "BO",
-  "Bosnia and Herzegovina": "BA",
-  "Botswana": "BW",
-  "Brazil": "BR",
-  "Brunei Darussalam": "BN",
-  "Bulgaria": "BG",
-  "Burkina Faso": "BF",
-  "Burundi": "BI",
-  "Cabo Verde": "CV",
-  "Cambodia": "KH",
-  "Cameroon": "CM",
-  "Canada": "CA",
-  "Central African Republic": "CF",
-  "Chad": "TD",
-  "Chile": "CL",
-  "China": "CN",
-  "Colombia": "CO",
-  "Comoros": "KM",
-  "Congo": "CG",
-  "Congo (Democratic Republic)": "CD",
-  "Costa Rica": "CR",
-  "Croatia": "HR",
-  "Cuba": "CU",
-  "Cyprus": "CY",
-  "Czechia": "CZ",
-  "Denmark": "DK",
-  "Djibouti": "DJ",
-  "Dominica": "DM",
-  "Dominican Republic": "DO",
-  "Ecuador": "EC",
-  "Egypt": "EG",
-  "El Salvador": "SV",
-  "Equatorial Guinea": "GQ",
-  "Eritrea": "ER",
-  "Estonia": "EE",
-  "Eswatini": "SZ",
-  "Ethiopia": "ET",
-  "Fiji": "FJ",
-  "Finland": "FI",
-  "France": "FR",
-  "Gabon": "GA",
-  "Gambia": "GM",
-  "Georgia": "GE",
-  "Germany": "DE",
-  "Ghana": "GH",
-  "Greece": "GR",
-  "Grenada": "GD",
-  "Guatemala": "GT",
-  "Guinea": "GN",
-  "Guinea-Bissau": "GW",
-  "Guyana": "GY",
-  "Haiti": "HT",
-  "Honduras": "HN",
-  "Hungary": "HU",
-  "Iceland": "IS",
-  "India": "IN",
-  "Indonesia": "ID",
-  "Iran": "IR",
-  "Iraq": "IQ",
-  "Ireland": "IE",
-  "Israel": "IL",
-  "Italy": "IT",
-  "Jamaica": "JM",
-  "Japan": "JP",
-  "Jordan": "JO",
-  "Kazakhstan": "KZ",
-  "Kenya": "KE",
-  "Kiribati": "KI",
-  "Kuwait": "KW",
-  "Kyrgyzstan": "KG",
-  "Laos": "LA",
-  "Latvia": "LV",
-  "Lebanon": "LB",
-  "Lesotho": "LS",
-  "Liberia": "LR",
-  "Libya": "LY",
-  "Liechtenstein": "LI",
-  "Lithuania": "LT",
-  "Luxembourg": "LU",
-  "Madagascar": "MG",
-  "Malawi": "MW",
-  "Malaysia": "MY",
-  "Maldives": "MV",
-  "Mali": "ML",
-  "Malta": "MT",
-  "Marshall Islands": "MH",
-  "Mauritania": "MR",
-  "Mauritius": "MU",
-  "Mexico": "MX",
-  "Micronesia": "FM",
-  "Moldova": "MD",
-  "Monaco": "MC",
-  "Mongolia": "MN",
-  "Montenegro": "ME",
-  "Morocco": "MA",
-  "Mozambique": "MZ",
-  "Myanmar": "MM",
-  "Namibia": "NA",
-  "Nauru": "NR",
-  "Nepal": "NP",
-  "Netherlands": "NL",
-  "New Zealand": "NZ",
-  "Nicaragua": "NI",
-  "Niger": "NE",
-  "Nigeria": "NG",
-  "North Korea": "KP",
-  "North Macedonia": "MK",
-  "Norway": "NO",
-  "Oman": "OM",
-  "Pakistan": "PK",
-  "Palau": "PW",
-  "Palestine": "PS",
-  "Panama": "PA",
-  "Papua New Guinea": "PG",
-  "Paraguay": "PY",
-  "Peru": "PE",
-  "Philippines": "PH",
-  "Poland": "PL",
-  "Portugal": "PT",
-  "Qatar": "QA",
-  "Romania": "RO",
-  "Russia": "RU",
-  "Rwanda": "RW",
-  "Saint Kitts and Nevis": "KN",
-  "Saint Lucia": "LC",
-  "Saint Vincent and the Grenadines": "VC",
-  "Samoa": "WS",
-  "San Marino": "SM",
-  "Saudi Arabia": "SA",
-  "Senegal": "SN",
-  "Serbia": "RS",
-  "Seychelles": "SC",
-  "Sierra Leone": "SL",
-  "Singapore": "SG",
-  "Slovakia": "SK",
-  "Slovenia": "SI",
-  "Solomon Islands": "SB",
-  "Somalia": "SO",
-  "South Africa": "ZA",
-  "South Korea": "KR",
-  "South Sudan": "SS",
-  "Spain": "ES",
-  "Sri Lanka": "LK",
-  "Sudan": "SD",
-  "Suriname": "SR",
-  "Sweden": "SE",
-  "Switzerland": "CH",
-  "Syria": "SY",
-  "Tajikistan": "TJ",
-  "Tanzania": "TZ",
-  "Thailand": "TH",
-  "Timor-Leste": "TL",
-  "Togo": "TG",
-  "Tonga": "TO",
-  "Trinidad and Tobago": "TT",
-  "Tunisia": "TN",
-  "Türkiye": "TR",
-  "Turkmenistan": "TM",
-  "Tuvalu": "TV",
-  "Uganda": "UG",
-  "Ukraine": "UA",
-  "United Arab Emirates": "AE",
-  "United Kingdom": "GB",
   "United States": "US",
-  "Uruguay": "UY",
-  "Uzbekistan": "UZ",
-  "Vanuatu": "VU",
-  "Vatican City": "VA",
-  "Venezuela": "VE",
+  "Eurozone (portugal,spain)": "EU",
+  "United Kingdom": "GB",
+  "India": "IN",
+  "Australia": "AU",
+  "Canada": "CA",
+  "Japan": "JP",
+  "China": "CN",
+  "Switzerland": "CH",
+  "New Zealand": "NZ",
+  "Sweden": "SE",
+  "Norway": "NO",
+  "Denmark": "DK",
+  "South Africa": "ZA",
+  "Singapore": "SG",
+  "Hong Kong": "HK",
+  "South Korea": "KR",
+  "Malaysia": "MY",
+  "Thailand": "TH",
+  "Indonesia": "ID",
+  "Philippines": "PH",
+  "Mexico": "MX",
+  "Brazil": "BR",
+  "Argentina": "AR",
+  "Russia": "RU",
+  "Turkey": "TR",
+  "United Arab Emirates": "AE",
+  "Saudi Arabia": "SA",
+  "Egypt": "EG",
+  "Nigeria": "NG",
+  "Pakistan": "PK",
   "Vietnam": "VN",
-  "Yemen": "YE",
-  "Zambia": "ZM",
-  "Zimbabwe": "ZW"
+  "Kenya": "KE",
+  "Colombia": "CO",
+  "Chile": "CL",
+  "Poland": "PL",
+  "Czechia": "CZ",
+  "Hungary": "HU",
+  "Romania": "RO",
+  "Israel": "IL",
+  "Jordan": "JO",
+  "Qatar": "QA",
+  "Kuwait": "KW",
+  "Oman": "OM",
 };
+
--- currencyconverter.js
+++ currencyconverter.js
@@ -5,7 +5,6 @@ sel = document.getElementsByClassName("options");
 text1 = document.getElementById("amount1");
 text2 = document.getElementById("amount2");
 
-let flag = 0;
 let f,t;
 let c = 0;
 for(let j of sel){
@@ -27,7 +26,7 @@ for(let j of sel){
     }
     c++;
 }
-exchangeamount(f,t);
+exchangeamount(f,t).then(()=>{});
 
 /*We cannot write console.log(rate) outside cause rate value is assigned when promised resolved sent by asynchronous function but 
 console.log(rate) is synchronous so it executes first when rate is still undefined. */
@@ -37,12 +36,15 @@ for(let i of sel){
         if(evt.target.id==='f'){
             image1.src = `https://flagsapi.com/${evt.target.value}/shiny/64.png`;
             f = evt.target.value;
+            if(text1.value.length>30){}
+            else{exchangeamount(f,t).then((res)=>{text2.value = res*text1.value});}
         }
         if(evt.target.id==='t'){
             image2.src = `https://flagsapi.com/${evt.target.value}/shiny/64.png`;
             t = evt.target.value;
+            if(text1.value.length>30){}
+            else{exchangeamount(f,t).then((res)=>{text1.value = text2.value/res});}
         }
-        exchangeamount(f,t);
     /*event is occuring on the select element not options , hence event.target contains all options of select,
     we use event.target.selectedIndex or event.target.options[event.target.selectedIndex]*/
     })
@@ -61,7 +63,8 @@ async function exchangeamount(f,t){
             for(let j in countryList){
                 if(countryList[j]===t){
                     // console.log(data[i.toLowerCase()][j.toLowerCase()]);
-                    document.getElementById("output").innerText = `1 ${i} = ${data[i.toLowerCase()][j.toLowerCase()]} ${j}`;
+                    document.getElementById("output").innerHTML = `1 ${i} = ${data[i.toLowerCase()][j.toLowerCase()]} ${j} <br>
+                    1 ${j} = ${1/(data[i.toLowerCase()][j.toLowerCase()])} ${i}`;
                     console.log(`Fetch Successful ${fth.status}`);
                     return data[i.toLowerCase()][j.toLowerCase()]; //return statement acting like break
                     // break; 
@@ -80,7 +83,7 @@ function checkValid(e){
                 return true;
             }
             //This line text1.value.indexOf('.')==-1 checks that initially no '.' was present or not,-1 means nothing was present
-            else if(e.key=='.' && txt1.value.indexOf('.')==-1){
+            else if(e.key=='.' && e.target.value.indexOf('.')==-1){
 
                 return true;
             }
@@ -105,8 +108,11 @@ text2.addEventListener("keydown",(e)=>checkValid(e));
 text1.addEventListener("input",(e)=>{inputValue(e)});
 text2.addEventListener("input",(e)=>{inputValue(e)});
 
+let lastEdited ;
+
 function inputValue(e){
         if(e.target == text1){
+            lastEdited = "left";
             exchangeamount(f,t).then((res)=>{
                 text2.value = res * e.target.value;
                 if(e.target.value.length>30){
@@ -116,12 +122,12 @@ function inputValue(e){
             })
         }
         else{
+            lastEdited = "right";
             exchangeamount(f,t).then((res)=>{
-                text1.value = res * e.target.value;
+                text1.value = e.target.value/res;
                 if(e.target.value.length>30){
                     e.target.value = NaN;
                     document.getElementById("second").innerText = "Please Enter a valid amount!!";
-                    flag = 1;
                 }
             })
         }
@@ -130,11 +136,11 @@ function inputValue(e){
 inter = document.getElementById("interchange");
 inter.addEventListener("click",()=>{
     inter.classList.toggle('buttonclass');
-    let temp = image1.src;
-    image1.src = image2.src;
-    image2.src = temp;
-
-    [sel.item(0).value ,sel.item(1).value] = [sel.item(1).value,sel.item(0).value]
+    // let temp = image1.src;
+    // image1.src = image2.src;
+    // image2.src = temp;
+    [image1.src, image2.src] = [image2.src, image1.src];
+    [sel.item(0).value ,sel.item(1).value] = [sel.item(1).value,sel.item(0).value];
 
     //we need to change only the value and innerText will automatically change.
     
@@ -142,19 +148,14 @@ inter.addEventListener("click",()=>{
 
     /*This logic can't be used coz changing value of selected element might not immeditaely update innerText of selected option. */
 
-    if(flag == 0){
-     k = t ;
-     t = f;
-     f = k;
-     exchangeamount(f,t);
-     let temporary = text1.value;
-     text1.value = text2.value;
-     text2.value = temporary;
-    }
-    else{
-     exchangeamount(f,t);   
-     let temporary = text1.value;
-     text1.value = text2.value;
-     text2.value = temporary;
-    }
+    [f,t] = [t,f];
+    
+    //  let temporary = text1.value;
+    //  text1.value = text2.value;
+    //  text2.value = temporary;
+    //  [text1.value, text2.value] = [text2.value, text1.value];
+     if(lastEdited=="left"){
+     text1.dispatchEvent(new Event("input")); //we want to value to be recalculatd based on from value after interchanging
+     lastEdited = "left";
+     }else if(lastEdited=="right"){text2.dispatchEvent(new Event("input"));lastEdited="right";} 
 })
